39b1eb674bfb32e4a071269dbca771b46b92b6c0
reddyprasade <reddyprasade@gmail.com>
2022-09-23 15:45:15 +0530

    Add the files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VulnerableRailsApp::Application.config.secret_token = 'b72d8d0b01800069031b261b834cd3714692a7a434f44052e8a1d506ac972a3405d89cd00f2642807667312a27928687d96d4da49c0dde01a66a406ce6b8ff44'
